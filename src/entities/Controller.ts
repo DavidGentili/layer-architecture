e754a6f5a019ab108480cb84dc19c80c814a758a
David Gentili <davidgenntili@gmail.com>
2025-06-18 11:03:41 -0300

    feat: se hace variable la estructura permitiendo asignar otros elementos

--- src/entities/Controller.ts
+++ src/entities/Controller.ts
@@ -2,9 +2,9 @@ import { IModel } from "@/types";
 
 
 export abstract class Controller {
-    protected readonly model: IModel;
+    protected readonly model?: IModel;
 
-    constructor(model: IModel) {
+    constructor(model?: IModel) {
         this.model = model;
     }
 }
--- src/entities/Model.ts
+++ src/entities/Model.ts
@@ -2,9 +2,9 @@ import { IRepostories } from "@/types";
 
 
 export abstract class Model {
-    protected readonly repository: IRepostories;
+    protected readonly repository?: IRepostories;
 
-    constructor(repository: IRepostories) {
+    constructor(repository?: IRepostories) {
         this.repository = repository;
     }
 }
